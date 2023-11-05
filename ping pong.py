3bd05ebd1d80639283fdd89f11fc95dae13d13a7
Gofster <listaigg@gmail.com>
2023-11-05 15:21:56 +0300

    сделал движение ракеток

--- ping pong.py
+++ ping pong.py
@@ -2,16 +2,48 @@ from pygame import *
 wins = 700
 winh =500
 FPS = 60
+
 window = display.set_mode((wins,winh))
 display.set_caption('PING_PONG')
 background = transform.scale(image.load('66.jpg'), (700,500))
-window.blit(background,(0, 0))
 
+class GameSprite(sprite.Sprite):
+    def __init__(self, player_image,player_x, player_y,size_x,size_y, player_speed):
+        super().__init__()
+        self.image = transform.scale(image.load(player_image),(size_x,size_y))
+        self.speed = player_speed
+        self.rect = self.image.get_rect()
+        self.rect.x = player_x
+        self.rect.y = player_y
+    def reset(self):
+        window.blit(self.image, (self.rect.x, self.rect.y))
 
 clock = time.Clock()
 
+class Player(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_s] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_w] and self.rect.y < 395:
+            self.rect.y += self.speed
+class Player2(GameSprite):
+    def update(self):
+        keys_pressed = key.get_pressed()
+        if keys_pressed[K_o] and self.rect.y > 5:
+            self.rect.y -= self.speed
+        if keys_pressed[K_l] and self.rect.y < 395:
+            self.rect.y += self.speed
+class Ball(GameSprite):
+    def update(self):         
+  
+       
+
+player = Player('65.PNG', 10,200,40,100,10)
+player2 = Player2('65.PNG', 650,200,40,100,10)
 
 
+finish = False
 game = True
 while game:
     for e in event.get():
@@ -20,5 +52,11 @@ while game:
         elif e.type ==  KEYDOWN:
             if e.key == K_SPACE:
                 player.fire()
+    if finish != True:
+        window.blit(background,(0, 0))
+        player.update()
+        player.reset()
+        player2.update()
+        player2.reset()
     display.update()
     clock.tick(FPS)
