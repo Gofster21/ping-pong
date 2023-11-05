4ee228a45d70493ed5b16856fb340c47fa0285cc
Gofster <listaigg@gmail.com>
2023-11-05 14:52:02 +0300

    написал основу

--- ping pong.py
+++ ping pong.py
@@ -1,1 +1,24 @@
-import pygame
+from pygame import *
+wins = 700
+winh =500
+FPS = 60
+window = display.set_mode((wins,winh))
+display.set_caption('PING_PONG')
+background = transform.scale(image.load('66.jpg'), (700,500))
+window.blit(background,(0, 0))
+
+
+clock = time.Clock()
+
+
+
+game = True
+while game:
+    for e in event.get():
+        if e.type == QUIT:
+            game = False
+        elif e.type ==  KEYDOWN:
+            if e.key == K_SPACE:
+                player.fire()
+    display.update()
+    clock.tick(FPS)
